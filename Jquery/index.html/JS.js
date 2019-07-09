d734a5d571a001ca554b68bbd9d3a7a93f533c63
linzijian <1031531798@qq.com>
2019-07-09 21:59:20 +0800

    jquery事件触发

---
+++ Jquery/index.html/JS.js
@@ -0,0 +1,46 @@
+// $(document).ready(function () {
+// 	$('#dd').on('blur', function(){			
+// 	alert('hello')
+// }); 失去焦点事件
+// })
+// $(document).ready(function () {
+// 	$('.button').on('focus', function(){			
+// 	document.write('hello') 获取焦点事件
+// 	
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('mousedown', function(){			
+// 	alert('hello') 鼠标点下触发事件
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('mouseup', function(){			
+// 	alert('hello') //鼠标抬起触发事件
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('mouseover', function(){			
+// 	alert('hello') //鼠标移动到某一区域触发事件
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('mouseout', function(){			
+// 	alert('hello') //鼠标离开某一区域触发事件
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('keydown', function(){			
+// 	alert('hello') //键盘按下触发事件
+// });
+// })
+// $(document).ready(function () {
+// 	$('#dd').on('keyup', function(){			
+// 	alert('hello') //键盘抬起触发事件
+// });
+//  })
+$(document).ready(function () {
+	$('#dd').on('change', function(){			
+	alert('hello') //检查某个值改变触发事件
+});
+})
