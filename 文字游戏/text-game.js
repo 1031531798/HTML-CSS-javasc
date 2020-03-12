92fb35ca57e64992a9273589854a5a98ec063a98
1031531798 <il>
2020-03-12 10:11:04 +0800

    CSS动画练习

--- 文字游戏/text-game.js
+++ 文字游戏/text-game.js
@@ -14,6 +14,9 @@ var vm = new Vue({
 			btnRight: "往湖边方向",
 			statement: " ",
 			textC:0,
+			userAttr:false,
+			userAttrList:false,
+			showText:true,
 		}
 	},
 	methods:{
@@ -28,7 +31,7 @@ var vm = new Vue({
 		},
 		okUser(){
 			this.user = {name:this.name,sex:this.sex,job:this.job}
-			this.statement="角色创建成功，点击此处开始游戏";
+			this.statement="角色创建成功，点击开始游戏";
 			this.one = false;
 			switch(this.job){
 				case "战士":{this.user.防御 = 30; this.user.敏捷 = 10; this.user.力量 = 30;this.user.HP=10;break}
@@ -36,6 +39,7 @@ var vm = new Vue({
 				case "猎人":{this.user.防御 = 20; this.user.敏捷 = 20; this.user.力量 = 20;this.user.HP=10;break}
 			}
 			document.getElementById("text").onclick = this.textClick;
+			this.userAttr = true;
 		},
 		textClick(){
 			let tts = ["这是一片神奇的土地，你将作为流浪的旅人，将领略这里的神奇与秘密。-------点击继续","日升月落，掩盖好已经熄灭的篝火，选择你前进的方向。","略作休整后，你爬上高处寻找前进的方向，发现东西边都有炊烟，你决定往哪走？"]
@@ -58,6 +62,7 @@ var vm = new Vue({
 				case "原地等待":{this.statement="屋外突然传来了密集的脚步声，你察觉不对，夺门而出，但是还是晚了，你已经被一群山贼包围了，明晃晃的斧头直奔你的头颅而来...你死了，死于山贼围攻";this.start = true;this.two=false;this.center ="重新开始";break};
 				case "留在此处":{this.statement="想了想还是留在这里吧。外面传来呼喊声，你被包围了。双拳难敌四手，你倒在血泊中...你死了！";this.start = true;this.two=false;this.center ="重新开始";break};
 				case "参观村子":{this.statement="你犹豫了下，点了点头，不知不觉在老者的带领下进去了一片阴影处，远处传来呼啸声，你被乱箭射死了！。";this.start = true;this.two=false;this.center ="重新开始";break};
+				case "进入村庄":{this.statement="这就是个正常的村庄，你在村庄里休息片刻，补给了物资，打听到山头有恐怖的老虎，继续往西走就能见到城镇。";this.btnLeft = "前往山头";this.btnRight = "继续往西";break};
 			}
 		},
 		btnR(){
@@ -65,10 +70,10 @@ var vm = new Vue({
 				case "往湖边方向":{this.btnLeft = "杀死水蟒";this.btnRight = "逃跑";this.statement = "你正观察着湖面，突然有条水蟒冲了出来，你要怎么办？";break}
 				case "逃跑":{this.run();break}
 				case "往西走":{this.statement="一番长途跋涉后，你眼前出现了一个村庄，勤劳的农民已经结束了一天的工作，村子里飘出着米饭的香甜。要进去吗？";this.btnLeft = "进入村庄";this.btnRight = "忍受饥饿";break};
-				case "暗中观察":{this.statement="谨慎的你决定在观察村庄，毕竟防范之心不可无。（HP-2）";this.btnLeft = "进入村子";this.user.HP = this.user.HP-2;break};
+				case "暗中观察":{this.statement="谨慎的你决定在观察村庄，毕竟防范之心不可无。一天过去了，一切正常。";this.btnLeft = "进入村庄";this.btnRight = "转身离开";break};
 				case "四处逛逛":{this.statement="四周环境十分简洁，你走进了厨房，鼻尖传来一丝血腥的气味，一番搜索，发现了一罐装满血液的陶罐。";this.btnLeft = "留在此处";this.btnRight = "趁机离开";break};
 				case "婉言谢绝":{this.statement="老者眼中闪过一丝失望，老者：那请先生你来参观一下我们的村子吧？";this.btnLeft = "参观村子";this.btnRight = "坚决离开";this.user.HP = this.user.HP-2;break};
-				case "坚决离开":{this.statement="你离开了这个村子，打算自己寻找个简单的过夜之处。-----一天过去了，今天请做出你的选择";this.btnLeft = "进入村庄";this.btnRight = "忍受饥饿";break};
+				case "坚决离开":{this.statement="你离开了这个村子，打算自己寻找个简单的过夜之处。-----天色已晚，今天请做出你的选择";this.btnLeft = "往山顶走";this.btnRight = "往山下走";break};
 				case "趁机离开":{this.statement="事出蹊跷，你翻身从后门溜走，跑路不出片刻，回头发现背后一群举着火把的山贼包围了之前那个屋子，擦了擦额头的虚汗，生死一念间。";this.btnLeft = "";this.btnRight = "忍受饥饿";break};
 			}
 		},
@@ -76,7 +81,7 @@ var vm = new Vue({
 			let tt = document.getElementById("text");
 			switch(this.job){
 				case "战士":{this.statement = "一番苦斗后杀死了野猪（HP-1）。获得野猪的皮（防御值+5）-----点击继续";this.two=false;this.user.防御 = this.user.防御+5;this.user.HP=this.user.HP-1;tt.onclick=this.textClick;break}
-				case "射手":{this.statement = "未能一击毙命，凶猛的野猪冲了上来，拼着受伤杀死了野猪（HP-2）。获得野猪的皮（防御值+5）-----点击继续";this.two=false;this.two=false;this.user.防御 = this.user.防御+5;this.HP=this.user.HP-2;tt.onclick=this.textClick;break}
+				case "射手":{this.statement = "未能一击毙命，凶猛的野猪冲了上来，拼着受伤杀死了野猪（HP-2）。获得野猪的皮（防御值+5）-----点击继续";this.two=false;this.two=false;this.user.防御 = this.user.防御+5;this.user.HP=this.user.HP-2;tt.onclick=this.textClick;break}
 				case "猎人":{this.statement = "凭借经验轻松杀死了野猪。获得野猪的皮（防御值+5）-----点击继续";this.two=false;this.two=false;this.user.防御 = this.user.防御+5;tt.onclick=this.textClick;break}
 			}
 		},
@@ -95,6 +100,15 @@ var vm = new Vue({
 				case "射手":{this.statement = "你逃跑了";this.two=false;this.two=false;tt.onclick=this.textClick;break}
 				case "猎人":{this.statement = "你逃跑了";this.two=false;this.two=false;tt.onclick=this.textClick;break}
 			}
+		},
+		attr(){
+			if(!this.userAttrList){
+				this.userAttrList = true;
+			}else{
+				this.userAttrList = false;
+			}
 		}
 	}
 })
+
+
